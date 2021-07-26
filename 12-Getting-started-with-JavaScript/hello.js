72dc27dd6e475f37e4dc73743f0c9c5227fc4ee3
Ahsan Habib Nahid <53366506+ahsanhabibnahid@users.noreply.github.com>
2021-07-26 23:46:13 +0600

    getting started

--- 12-Getting-started-with-JavaScript/hello.js
+++ 12-Getting-started-with-JavaScript/hello.js
@@ -1,4 +1,54 @@
+// find variable type
+
+// var weight = 65;
+// console.log(typeof weight);
+
+// var day =  'sunday';
+// console.log(typeof day);
+
+// var cool = false;
+// console.log(typeof cool)
+
+// var hot = true;
+// console.log(typeof hot)
+
+// var lang = "Javascript"
+// console.log(lang.toLowerCase())
+// console.log(lang.toUpperCase())
+
+// Find Index
+
+// var myName = "ahsan habib nahid"
+// console.log (myName.indexOf('na'))
+
+// Split word
+
+// var lang = "JavaScript PHP Java Python C++"
+// console.log(lang.split(' '))
+
+// String to Number
+
+// var num1 = '20054';
+// num1 = +num1;
+// console.log(typeof num1)
+
+// var num2 = '578.65';
+// num2 = parseFloat(num2);
+// console.log(typeof num2)
+
+// Number to String
+
+// var num1 = 3658;
+// num1 = ''+num1;
+// console.log(typeof num1)
+
+// Float extra value fixed
+
 var num1 = 0.1
 var num2 = 0.2
 
-console.log((num1+num2).toFixed(1))
+var sum = num1 + num2
+sum = sum.toFixed(1)
+
+console.log(sum)
+
